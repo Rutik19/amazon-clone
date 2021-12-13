d1cf1461a73296a6611f04993dc836f4b5c6f462
abcd <abcd>
2021-12-13 21:29:56 +0530

    init

--- src/reducer.js
+++ src/reducer.js
@@ -3,22 +3,20 @@ export const initialState = {
 };
 
 //Selector
-export const getCartTotal = (cart) => 
-  cart?.reduce((amount, item)=> amount + item.price, 0);
-     
+export const getCartTotal = (cart) =>
+  cart?.reduce((amount, item) => amount + item.price, 0);
 
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
-      case "ADD_TO_CART":
-          return{
-              ...state,
-              cart: [...state.cart, action.item],
-          };
-      default:
-          return state;
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, action.item],
+      };
+    default:
+      return state;
   }
-
 };
 
 export default reducer;
